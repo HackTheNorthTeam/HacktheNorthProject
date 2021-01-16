b08457497a77c57ea383a6783763306dbb0007d2
ceezuns <ceezuns@gmail.com>
2021-01-16 16:21:48 -0500

    Replace plain implementation of WebRTC with library.

--- htnproject/static/js/index.js
+++ htnproject/static/js/index.js
@@ -1,126 +1,41 @@
-let configuration = {
-    iceServers: [
-        {
-            'urls': 'stun:stun.l.google.com:19302'
-        },
-        {
-            'urls': 'turn:numb.viagenie.ca',
-            'credential': 'HTNProject',
-            'username': 'HTNProject@mailpoof.com'
-        },
-    ]
-};
-
-let constraints = {
-    'video': true,
-    'audio': true
-}
+var local = document.getElementById('local');
+var remote = document.getElementById('remote');
+var connection = new RTCMultiConnection();
 
 const roomName = JSON.parse(document.getElementById('room-name').textContent);
-var socket = new WebSocket(
-    (location.protocol == "https:" ? "wss" : "ws") + "://" +
-    location.host +
-    "/ws/video/" +
-    roomName
-);
-var connection = new RTCPeerConnection(configuration);
+connection.socketUrl = connection.socketURL = 'https://rtcmulticonnection.herokuapp.com:443/';
 
-socket.onopen = event => {
-    console.log('Opened socket!');
+connection.iceServers = [];
+connection.iceServers.push({
+    urls: 'stun:stun.l.google.com:19302'
+});
+connection.iceServers.push({
+    urls: 'turn:numb.viagenie.ca:3478',
+    credential: 'HTNProject',
+    username: 'HTNProject@mailpoof.com'   
+});
+
+connection.session = {
+    audio: true,
+    video: true
+};
+connection.sdpConstraints.mandatory = {
+    OfferToReceiveAudio: true,
+    OfferToReceiveVideo: true
 };
 
-function createIceCandidate(event) {
-    if (event.candidate) {
-        socket.send(JSON.stringify({
-            "type": "send_ice_candidate",
-            "data": event.candidate
-        }));
+connection.onstream = function(event) {
+    switch (event.type) {
+        case 'local':
+            local.appendChild(event.mediaElement);
+            break;
+        case 'remote':
+            remote.appendChild(event.mediaElement);
+            break;
     }
-}
-
-function addStream(event) {
-    remote.srcObject = event.stream;
-}
-
-connection.onicecandidate = createIceCandidate;
-connection.onaddstream = addStream;
-
-var local = document.getElementById("local");
-var remote = document.getElementById("remote");
-var stream;
-navigator
-    .mediaDevices
-    .getUserMedia(constraints)
-    .then(stream => {
-        this.stream = stream;
-    })
-    .catch(error => console.error("Failed to get user media stream.", error));
-
+};
 
 document.querySelector('#room-name-submit').onclick = function (event) {
-    handleNegotiationNeededEvent();
-    console.log("Sending offer");
-};
-
-socket.onmessage = function (event) {
-    const data = JSON.parse(event.data);
-    console.log(data);
-    if (data['type'] == 'offer') {
-        console.log();
-        handleVideoOfferMessage(data)
-        console.log('Got to send answer part.');
-    } else if (data['type'] == 'answer') {
-        console.log('Got to send offer part.');
-    } else if (data['type'] == 'ice') {
-        connection.addIceCandidate(new RTCIceCandidate(data));
-    }
-};
-
-function handleNegotiationNeededEvent() {
-    connection.createOffer()
-    .then(offer => {
-        return connection.setLocalDescription(offer);
-    })
-    .then(() => {
-        socket.send(JSON.stringify({
-            "type": "send_offer",
-            "data": connection.localDescription
-        }));
-      })
-      .catch(error => console.error("Error with offer sending", error));
-}
-
-function handleVideoOfferMessage(offer) {
-    connection.onicecandidate = function (event) {
-        socket.send(JSON.stringify({
-            "type": "send_ice_candidate",
-            "data": event.candidate
-        }));
-    }
-    connection.onaddstream = function (event) {
-        remote.srcObject = event.stream;
-    }
-    connection.setRemoteDescription(new RTCSessionDescription(offer));
-    connection.createAnswer()
-    .then(answer => {
-        return connection.setLocalDescription(answer);
-    })
-    .then(() => {
-        socket.send(JSON.stringify({
-            "type": "send_answer",
-            "data": connection.localDescription
-        }));
-    })
-
-    .then(() => {
-        const stream = new MediaStream();
-        connection.addEventListener('track', async (event) => {
-            stream.addTrack(event.track, stream);
-        });
-        remote.srcObject = stream;
-    })
-}
-
-socket.onclose = event => {
-    console.error('Chat socket closed unexpectedly');
+    console.log("Joining room!")
+    connection.openOrJoin(roomName);
 };
