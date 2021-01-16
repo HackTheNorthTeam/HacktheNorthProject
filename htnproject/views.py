1754e3e41f360e8976d5c2ff2f008ecfa4e721ea
BuildTools <lucab.mohabir@gmail.com>
2021-01-16 12:10:04 -0500

    Began implementing authentication and continued improving UI

--- htnproject/views.py
+++ htnproject/views.py
@@ -1,4 +1,19 @@
+from django.http import HttpResponse
+from django.http.response import HttpResponseServerError
+from django.template import loader
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
 def index(request):
     return render(request, 'index.html')
+
+def authenticateUser(request):   
+    username = request.POST['username']
+    password = request.POST['passwd']
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+
+    return HttpResponse    
+
